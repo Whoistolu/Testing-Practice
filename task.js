38ca6705f3c05a64d5fe2b7a20ad3f67a7c625e6
Whoistolu <digittolu25@gmail.com>
2021-09-30 08:00:27 +0100

    Add test for a calculator that adds, substracts, multiplies && divides integers

--- task.js
+++ task.js
@@ -9,12 +9,37 @@ return string.length
 const reverseString = (string) => {
     let reversedString = string.toLowerCase().split('').reverse().join('');
     return reversedString;
-}
+};
+
+
+
+class integerCalculator {
+    constructor(firstNumber, secondNumber) {
+      this.firstNumber = firstNumber;
+      this.secondNumber = secondNumber;
+    }
+  
+    addNumbers() {
+      return this.firstNumber + this.secondNumber;
+    }
+  
+    subtractNumbers() {
+      return this.firstNumber - this.secondNumber;
+    }
+  
+    multiplyNumbers() {
+      return this.firstNumber * this.secondNumber;
+    }
+  
+    divideNumbers() {
+      const quotient = (this.firstNumber / this.secondNumber).toFixed(2);
+      return parseFloat(quotient, 10);
+    }
+  }
 
 
 
 
 
 
-
-module.exports = { stringLength, reverseString };
+module.exports = { stringLength, reverseString, integerCalculator };
--- task.test.js
+++ task.test.js
@@ -1,4 +1,4 @@
-const { stringLength, reverseString } = require('./task');
+const { stringLength, reverseString, integerCalculator } = require('./task');
 
 describe('All tests for string length', () => {
     test('return length of string', () => {
@@ -32,5 +32,104 @@ describe('Return a reversed string', () => {
   
       // Assert
       expect(stringisReversed).toBe('mas');
+    });
+});
+
+describe('Basic mathematical Operations', () => {
+    describe('Addition of integers', () => {
+      test('Add two numbers (1, 2) to get a sum (3)', () => {
+        // Arrange and Act
+        const addition = new integerCalculator(1, 2);
+        
+        // Assert
+        expect(addition.addNumbers()).toEqual(3);
+      })
+      test('Add two numbers(2, 2) to get a sum(4)', () => {
+        // Arrange and Act
+        const addition = new integerCalculator(2, 2);
+        
+        // Assert
+        expect(addition.addNumbers()).toEqual(4);
+      })
+      test('Add two numbers(100, 100) to get a sum(200)', () => {
+        // Arrange and Act
+        const addition = new integerCalculator(100, 100);
+        
+        // Assert
+        expect(addition.addNumbers()).toEqual(200);
+      })
     })
-  })
+  
+    describe('Subtraction of integers', () => {
+      test('Subtract a number(1) from (6) to get difference (-1)', () => {
+        // Arrange and Act
+        const subtraction = new integerCalculator(1, 6);
+        
+        // Assert
+        expect(subtraction.subtractNumbers()).toEqual(-5);
+      })
+      test('Subtract a number(6) from (6) to get a difference (0)', () => {
+        // Arrange and Act
+        const subtraction = new integerCalculator(6, 6);
+        
+        // Assert
+        expect(subtraction.subtractNumbers()).toEqual(0);
+      })
+      test('Subtract a number(8) from (6) to get a difference (2)', () => {
+        // Arrange and Act
+        const subtraction = new integerCalculator(8, 6);
+        
+        // Assert
+        expect(subtraction.subtractNumbers()).toEqual(2);
+      })
+    })
+  
+    describe('Multiplication of integers', () => {
+      test('Multiply a number(4) by (6) to get a product (24)', () => {
+        // Arrange and Act
+        const multiplication = new integerCalculator(4, 6);
+        
+        // Assert
+        expect(multiplication.multiplyNumbers()).toEqual(24);
+      })
+      test('Multiply a number(7) by (100) to get a product (700)', () => {
+        // Arrange and Act
+        const multiplication = new integerCalculator(7, 100);
+        
+        // Assert
+        expect(multiplication.multiplyNumbers()).toEqual(700);
+      })
+      test('Multiply a number(10) by (1) to get a product (10)', () => {
+        // Arrange and Act
+        const multiplication = new integerCalculator(10, 1);
+        
+        // Assert
+        expect(multiplication.multiplyNumbers()).toEqual(10);
+      })
+    })
+  
+    describe('Division of integers', () => {
+      test('Divide a number(5) by (6) to get a quotient (0.83)', () => {
+        // Arrange and Act
+        const division = new integerCalculator(5, 6);
+        
+        // Assert
+        expect(division.divideNumbers()).toEqual(0.83);
+      })
+      test('Divide a number(10) by (2) to get a quotient (5)', () => {
+        // Arrange and Act
+        const division = new integerCalculator(10, 2);
+        
+        // Assert
+        expect(division.divideNumbers()).toEqual(5);
+      })
+      test('Divide a number(100) by (10) to get a quotient (10)', () => {
+        // Arrange and Act
+        const division = new integerCalculator(100, 10);
+        
+        // Assert
+        expect(division.divideNumbers()).toEqual(10);
+      });
+    });
+  });
+
